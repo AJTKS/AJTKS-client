9f50441e34914b5c25c7bfcce498221a3f65a611
Junhyeong Park <pljh0906@yonsei.ac.kr>
2024-05-17 01:26:49 +0900

    fix: https to http

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -29,7 +29,7 @@ const MainPage: React.FC = () => {
 
     try {
       const response = await axios.post(
-        "http://58.233.143.58:5000/upload",
+        "https://58.233.143.58:5000/upload",
         formData,
         {
           headers: {
@@ -44,7 +44,7 @@ const MainPage: React.FC = () => {
       const checkTaskStatus = async () => {
         try {
           const statusResponse = await axios.get(
-            `http://58.233.143.58:5000/task/${task_id}`
+            `https://58.233.143.58:5000/task/${task_id}`
           );
           const { status, searchResult } = statusResponse.data;
 
