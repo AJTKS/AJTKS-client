2da02a7318eabde1926220e03dab7b360052f0e0
Junhyeong Park <pljh0906@yonsei.ac.kr>
2024-05-22 13:07:01 +0900

    fix: loading duartion

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -44,7 +44,7 @@ const MainPage: React.FC = () => {
 
       setTimeout(() => {
         navigate("/result", { state: { taskId: task_id, fileName } });
-      }, 6000);
+      }, 6500);
     } catch (error) {
       console.error("Error uploading file:", error);
       setIsAnalyzing(false);
